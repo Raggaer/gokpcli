a7e6a5548ada3ace6c72db2746cd10d1df312d8f
Raggaer <nakotoffana@gmail.com>
2019-02-23 17:17:27 +0100

    Fix no backups flag

--- save.go
+++ save.go
@@ -20,7 +20,7 @@ func databaseChangedSaveAlert(f *form, answer string) {
 		activeForm = nil
 	}()
 	// First we backup the database
-	if doNotBackups {
+	if !doNotBackups {
 		if err := backupDatabase(); err != nil {
 			fmt.Println("Unable to backup database:")
 			fmt.Println(err.Error())
