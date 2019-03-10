0066e6fd5b9200b1cc85e882e5216da113c227a4
Raggaer <nakotoffana@gmail.com>
2019-03-11 00:23:55 +0100

    Fix show command

--- entry.go
+++ entry.go
@@ -190,7 +190,7 @@ func show(args []string) {
 	fmt.Println("Password: " + mask(entry.GetPassword(), "*"))
 	fmt.Println("URL: " + entry.GetContent("URL"))
 	fmt.Println("Notes:")
-	if len(entry.GetContent("notes")) > 0 {
+	if len(entry.GetContent("Notes")) > 0 {
 		fmt.Println(entry.GetContent("Notes"))
 	}
 }
